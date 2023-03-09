e6314a91ead364c4768ed98da7098944091be074
Cheikh White <cheikh.white@GL08920M.local>
2023-03-09 00:31:41 +0000

    Adding correct egress rules to allow internet access

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "otraw_security_group" {
   name_prefix = "otraw_security_group"
   vpc_id = aws_vpc.otraw_vpc.id
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -46,11 +53,17 @@ resource "aws_security_group" "otraw_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+#   ingress {
+#   from_port = 443
+#   to_port = 443
+#   protocol = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+# }
 }
 
 # Create a network interface for the instance
